f52a9d81f78eb0f9b149144c10af73fca3a22570
mmscibor <mmscibor@gmail.com>
2014-02-01 15:08:11 -0500

    Small patch, Deck construction is fine

--- src/Deck.java
+++ src/Deck.java
@@ -1,23 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Deck {
 
     /* Deck Object: represents a collection of all the cards necessary to play the game. */
 
-    final int DECK_SIZE = 81; // 3^4, number of cards in the deck
     final int COLORS = 3, SHAPES = 3, SHADES = 3, QUANTITIES = 3;
 
-    Card[] cardsInDeck = new Card[DECK_SIZE];
-    // TODO: I made this an Array, a Stack may be more appropriate?
+    List<Card> cardsInDeck = new ArrayList<Card>();
 
     public Deck() {
-        // TODO: Quadruple nested for loop, not costly because 81 runs, but would be nice to have something more efficient
+        // Populate the Deck.
         for (int color = 0; color < COLORS; color++) {
             for (int shape = 0; shape < SHAPES; shape++) {
                 for (int shade = 0; shade < SHADES; shade++) {
                     for (int quantity = 0; quantity < QUANTITIES; quantity++) {
-                        int indexInDeck = 3 * 3 * 3 * color + 3 * 3 * shape + 3 * shade + quantity;
-                        cardsInDeck[indexInDeck] = new Card(color, shape, shade, quantity);
+                        cardsInDeck.add(new Card(color, shape, shade, quantity));
                     }
                 }
             }
@@ -26,20 +25,20 @@ public class Deck {
 
     // This method retrieves a particular card in the deck
     public Card getCard(int index) {
-        return cardsInDeck[index];
+        return cardsInDeck.get(index);
     }
 
     public int deckSize() {
-        return cardsInDeck.length;
+        return cardsInDeck.size();
     }
 
     public void shuffleDeck() {
         Random random = new Random();
-        for (int i = cardsInDeck.length - 1; i > 0; i--){
+        for (int i = cardsInDeck.size() - 1; i > 0; i--){
             int index = random.nextInt(i+1);
-            Card placeholder = cardsInDeck[index];
-            cardsInDeck[index] = cardsInDeck[i];
-            cardsInDeck[i] = placeholder;
+            Card placeholder = cardsInDeck.get(index);
+            cardsInDeck.set(index, cardsInDeck.get(i));
+            cardsInDeck.set(i, placeholder);
         }
     }
 }
--- src/GameSet.java
+++ src/GameSet.java
@@ -1,16 +1,17 @@
 import java.util.List;
 
 public class GameSet {
-    public static void main(String[] args){
-        Deck deck = new Deck(); // See Deck class for details.
+    public static void main(String[] args) {
+        Deck deck = new Deck(); // Create a Deck of 81 cards.
+        deck.shuffleDeck();
     }
 
     // This method takes as an input a List<Card> and confirms whether or not it's a set
-    private void confirmCards(List<Card> cardTriplet){
+    public static boolean confirmCards(List<Card> cardTriplet) {
         // First check to be certain that three cards have been selected
-        if (cardTriplet.size() != 3){
+        if (cardTriplet.size() != 3) {
             // TODO: Here it will print a message saying that an invalid selection has been made
-            return; // This ends the method without running the rest of it
+            return false; // This ends the method without running the rest of it
         }
 
         // If we do in fact have exactly 3 cards, the below code is valid.
@@ -25,14 +26,18 @@ public class GameSet {
         int[] secFeat = secondCard.getFeatures();
         int[] thirdFeat = thirdCard.getFeatures();
 
-        boolean confirmation; // We will set this TRUE if it is a SET and FALSE otherwise.
+        boolean confirmation = true; // We will set this TRUE if it is a SET and FALSE otherwise.
 
-        for(int feature = 0; feature < firstFeat.length; feature++){
+        for (int feature = 0; feature < firstFeat.length; feature++) {
             // Case where all 3 features are equal to each other
-            if (firstFeat[feature] == secFeat[feature] && firstFeat[feature] == thirdFeat[feature]){
-                // TODO: This is incomplete, finish.
+            boolean allFeatEqual = firstFeat[feature] == secFeat[feature] && firstFeat[feature] == thirdFeat[feature];
+            boolean allFeatUnequal = firstFeat[feature] != secFeat[feature] && firstFeat[feature] !=
+                    thirdFeat[feature] && secFeat[feature] != thirdFeat[feature];
+
+            if (!(allFeatEqual || allFeatUnequal)) {
+                confirmation = false;
             }
         }
+        return confirmation;
     }
-
 }
