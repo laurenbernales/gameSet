916caa11554afbbb4ca42712eceeafa864399b15
jenniferli78 <jenniferli.email@yahoo.com>
2014-02-07 23:17:46 -0500

    Worked on checkForSetsOnBoard, changed name to noSetsOnBoard.

--- src/GameSet.java
+++ src/GameSet.java
@@ -5,12 +5,26 @@ public class GameSet {
     public static void main(String[] args) {
         Deck deck = new Deck(); // Create a Deck of 81 cards.
 
-        List<Card> cardSet = new ArrayList<Card>();
-        cardSet.add(deck.distributeCard());
-        cardSet.add(deck.distributeCard());
-        cardSet.add(deck.distributeCard());
+        //testing for confirmCards
+        //List<Card> cardSet = new ArrayList<Card>();
+        //cardSet.add(deck.distributeCard());
+        //cardSet.add(deck.distributeCard());
+        //cardSet.add(deck.distributeCard());
 
-        System.out.println(Player.confirmCards(cardSet));
+        //System.out.println(Player.confirmCards(cardSet));
+        
+        //testing for noSetsOnBoard
+        List<Card> cardSet1 = new ArrayList<Card>();
+        cardSet1.add(new Card(0,0,0,0));
+        cardSet1.add(new Card(1,0,0,0));
+        cardSet1.add(new Card(1,1,0,0));
+        cardSet1.add(new Card(1,1,1,0));
+        cardSet1.add(new Card(1,1,1,1));
+        cardSet1.add(new Card(2,1,1,1));
+        cardSet1.add(new Card(2,2,1,1));
+        cardSet1.add(new Card(2,2,2,1));          
+        
+        System.out.println(noSetsOnBoard(cardSet1));
 
         // TODO: Network interface, see how many players are in the game.
         int numPlayers = 0;
@@ -20,14 +34,32 @@ public class GameSet {
         for (int i = 0; i < numPlayers; i++) {
             playersInGame.add(new Player(playerName));
         }
-
     }
-
-    public static boolean checkForSetsOnBoard(List<Card> cardsInPlay) {
-        // TODO: If there are no sets return true
-        return true;
-    }
-
+    
+    public static boolean noSetsOnBoard(List<Card> cardsInPlay) {
+        //If there are no sets return true
+    	boolean containsnoset = true; 
+    	for (int i = cardsInPlay.size()-1; i > -1; i--){
+    		for (int j = cardsInPlay.size()-1; j > -1; j--){
+    			for (int k = cardsInPlay.size()-1; k > -1; k--){
+    				//all cards should be different 
+    				if (i != k && i != j && k != j){
+    			        List<Card> cardSet = new ArrayList<Card>();
+    			        cardSet.add(cardsInPlay.get(i));
+    			        cardSet.add(cardsInPlay.get(j));
+    			        cardSet.add(cardsInPlay.get(k));
+    					boolean containset = Player.confirmCards(cardSet);
+    					if (containset == true){
+    						containsnoset = false; 
+    					}
+    				}
+    			}
+    		}	
+    	}
+    	return containsnoset;
+    } 
+      
+    
     public static boolean isThereMoreThanTwelve(List<Card> cardsInPlay) {
         if (cardsInPlay.size() > 12) {
             return true;
