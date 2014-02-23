a5c4c66e047be4881214cde9984c6508b6111b52
mmscibor <mmscibor@gmail.com>
2014-02-23 16:07:22 -0500

    Set up a Global Variables class

--- src/GameSet.java
+++ src/GameSet.java
@@ -18,19 +18,19 @@ public class GameSet {
 
     public static boolean noSetsOnBoard(List<Card> cardsInPlay) {
         //If there are no sets return true
-        // TODO: Improve this to run like O(N^2) instead of O(N^3)
+        // TODO: OPTIONAL: Improve this to run like O(N^2) instead of O(N^3)
         boolean containsNoSet = true;
+        List<Card> cardSet;
         for (int i = cardsInPlay.size() - 1; i > -1; i--) {
             for (int j = cardsInPlay.size() - 1; j > -1; j--) {
                 for (int k = cardsInPlay.size() - 1; k > -1; k--) {
                     //all cards should be different
                     if (i != k && i != j && k != j) {
-                        List<Card> cardSet = new ArrayList<Card>();
+                        cardSet = new ArrayList<Card>();
                         cardSet.add(cardsInPlay.get(i));
                         cardSet.add(cardsInPlay.get(j));
                         cardSet.add(cardsInPlay.get(k));
-                        boolean containSet = Player.confirmCards(cardSet);
-                        if (containSet == true) {
+                        if (Player.confirmCards(cardSet)) {
                             containsNoSet = false;
                         }
                     }
