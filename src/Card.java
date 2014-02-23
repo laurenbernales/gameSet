df7c216adc6e79eaaeec6de951bda547c184c8de
mmscibor <mmscibor@gmail.com>
2014-02-23 10:57:36 -0500

    Changed some code

--- src/Card.java
+++ src/Card.java
@@ -10,7 +10,7 @@ public class Card {
 
     int color, shape, quantity, shade;
 
-    public Card(int color, int shape, int shade, int quantity){
+    public Card(int color, int shape, int shade, int quantity) {
         this.color = color;
         this.shade = shade;
         this.shape = shape;
@@ -19,83 +19,71 @@ public class Card {
 
     // Getter method, returns the 4 features of a card in an array
 
-    public int[] getFeatures(){
+    public int[] getFeatures() {
         int[] featureArray = {color, shape, shade, quantity};
         return featureArray;
     }
-    
-    
-    //don't have the images, but setting it up anyways
-    //It's kind of long, but each feature would correspond to a different 
-    //string, so haven't figured out how to make more efficient
-    public String getImageName(){
-    	//color
-    	int feature1 = getFeatures()[0];
-    	//String stringFeature1 = String.valueOf(feature1);
-    	String color = "";
-    	if (feature1 == 0){
-    		color = "color:zero_";
-    	}
-    	else if (feature1 == 1){
-    		color = "color:one_";
-    	}
-    	else if (feature1 == 2){
-    		color = "color:two_";
-    	}
-    	else{
-    		color = "color:three_";
-    	}
 
-        //shape
-    	int feature2 = getFeatures()[1];
-    	String shape = "";
-    	if (feature2 == 0){
-    		shape = "shape:zero_";
-    	}
-    	else if (feature2 == 1){
-    		shape = "shape:one_";
-    	}
-    	else if (feature2 == 2){
-    		shape = "shape:two_";
-    	}
-    	else{
-    		shape = "shape:three_";
-    	}
+    public String getImageName() {
+        String colorString, shapeString, shadeString, quantityString;
 
-    	//shade
-    	int feature3 = getFeatures()[2];
-    	String shade = "";
-    	if (feature3 == 0){
-    		shade = "shade:zero_";
-    	}
-    	else if (feature3 == 1){
-    		shade = "shade:one_";
-    	}
-    	else if (feature3 == 2){
-    		shade = "shade:two_";
-    	}
-    	else{
-    		shade = "shade:three_";
-    	}
-    	
-    	//quantity
-    	int feature4 = getFeatures()[3];
-    	String quant = "";
-    	if (feature4 == 0){
-    		quant = "quant:zero";
-    	}
-    	else if (feature4 == 1){
-    		quant = "quant:one";
-    	}
-    	else if (feature4 == 2){
-    		quant = "quant:two";
-    	}
-    	else{
-    		quant = "quant:three";
-    	}
-    	String name = color+shape+shade+quant+".png" ;
-    	return name;
+        switch (color) {
+            case 0:
+                colorString = "color:zero_";
+                break;
+            case 1:
+                colorString = "color:one_";
+                break;
+            case 2:
+                colorString = "color:two_";
+                break;
+            default:
+                colorString = "color:null_";
+        }
+
+        switch (shape) {
+            case 0:
+                shapeString = "shape:zero_";
+                break;
+            case 1:
+                shapeString = "shape:one_";
+                break;
+            case 2:
+                shapeString = "shape:two_";
+                break;
+            default:
+                shapeString = "shape:null_";
+        }
+
+        switch (shade) {
+            case 0:
+                shadeString = "shade:zero_";
+                break;
+            case 1:
+                shadeString = "shade:one_";
+                break;
+            case 2:
+                shadeString = "shade:two_";
+                break;
+            default:
+                shadeString = "shade:null_";
+        }
+
+        switch (quantity) {
+            case 0:
+                quantityString = "quantity:zero_";
+                break;
+            case 1:
+                quantityString = "quantity:one_";
+                break;
+            case 2:
+                quantityString = "quantity:two_";
+                break;
+            default:
+                quantityString = "quantity:null_";
+        }
+
+        return (colorString + shapeString + shadeString + quantityString + ".png");
     }
-    
 }
 
